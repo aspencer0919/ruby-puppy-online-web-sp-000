c771f761f9c19ae5c42d62c61ce7c3c2681e0b3c
Adam Spencer <aspencer0919@gmail.com>
2020-04-14 23:09:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,9 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << self
+    def save
+      @@all << self
+    end
   end
 
   def self.all
@@ -19,8 +21,5 @@ class Dog
     @@all.each do |x|
       puts x.name
     end
-  end
-
-  def new
   end
 end
